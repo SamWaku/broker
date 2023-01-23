59f3666ce24b1fd56f79eaa22beaac776f9a108e
ngalande <blessingsngalande2001@gmail.com>
2023-01-23 13:16:00 +0200

    init

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ require('dotenv').config();
 // const to = require('await-to-js').default;
 const broker = require('./src/broker/broker');
 
-const server = require('net').createServer(broker.handle)
-const port = 8883;
+// const server = require('net').createServer(broker.handle)
+const server = require('net').createServer(broker.handle);
+const port = process.env.PORT ? process.env.PORT : 8883;
 
 (async () => {
 
