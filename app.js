89ecf02253bbf9a11e315037c8280a233d03dcc2
ngalande <blessingsngalande2001@gmail.com>
2023-01-23 11:38:42 +0200

    init

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 require('dotenv').config();
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
 
 // const to = require('await-to-js').default;
 const broker = require('./src/broker/broker');
@@ -6,12 +13,15 @@ const broker = require('./src/broker/broker');
 const server = require('net').createServer(broker.handle)
 const port = 8883;
 
-(async () => {
-
-  server.
-
-  server.listen(port, '0.0.0.0', function () {
+app.listen(port, '0.0.0.0', function () {
     console.log('🚀  Server listening on port: ', port)
   });
-})();
+// (async () => {
+
+//   server.
+
+//   server.listen(port, '0.0.0.0', function () {
+//     console.log('🚀  Server listening on port: ', port)
+//   });
+// })();
 
