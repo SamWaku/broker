d6f95c22b7808797c09ac8060f68faa34978430a
blessings ngalande <blessngalande@gmail.com>
2023-09-25 04:59:20 -0500

    update

--- src/broker/clientMsgHandler.js
+++ src/broker/clientMsgHandler.js
@@ -1,5 +1,6 @@
 // const mq = require('../client')
 var amqp = require('amqplib/callback_api');
+const WebSocket = require('ws');
 
 //function to handle emiting messages to rabbitmq
 function HandleMsg(topic, msg){
@@ -9,27 +10,21 @@ function HandleMsg(topic, msg){
             break;
         case 'etoll/rfid':
             // console.log(" Pappi Sent %s", msg);
-            //connect to amqp and send queue
-            amqp.connect(process.env.AMQP_URL, function(error0, connection) {
-                if (error0) {
-                  throw error0;
-                }
-                connection.createChannel(function(error1, channel) {
-                  if (error1) {
-                    throw error1;
-                  }
-                  var queue = 'etoll';
-              
-                  channel.assertQueue(queue, {
-                    durable: false
-                  });
-                  // called each time a message is received
-                rfid=msg
-                //message sent to amqp
-                channel.sendToQueue(queue, Buffer.from(rfid));
-                console.log(" [x] Sent to AMQP %s", rfid);
-              })
-            })
+            //connect to websocket
+            // Create a WebSocket client
+            const client = new WebSocket('ws://localhost:8080');
+
+            // Event handler for when the client connection is established
+            client.on('open', () => {
+              console.log('Connected to the server.');
+
+              // Send data to the server
+              client.send(msg);
+
+              // Close the connection after sending the message
+              client.close();
+            });
+            
             break;
 
         default: break;
