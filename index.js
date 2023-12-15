62480246abf75c9f282a55b77fe525113584ef7c
SamWaku <56162251+SamWaku@users.noreply.github.com>
2023-12-15 07:12:59 +0200

    initial

--- index.js
+++ index.js
@@ -1,96 +1,65 @@
-const express = require('express')
-const mqtt = require('mqtt')
-require('dotenv').config();
+const express = require("express");
+const mqtt = require("mqtt");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 var options = {
-    host: process.env.MQTT_HOST,
-    port: process.env.MQTT_PORT,
-    protocol: process.env.MQTT_PROTOCAL,
-    username: process.env.MQTT_USERNAME,
-    password: process.env.MQTT_PASSWORD,
-    clientId: 'mqttjs_pappi',
-    clean: false,
-}
+  host: process.env.MQTT_HOST,
+  port: process.env.MQTT_PORT,
+  protocol: process.env.MQTT_PROTOCAL,
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD,
+  clientId: "mqttjs_pappi",
+  clean: false,
+};
 
 // initialize the MQTT client
 var client = mqtt.connect(options);
-var arr=[]
-var data=''
+var arr = [];
+var data = "";
 let count = 0;
 let prevCount = 0;
 // setup the callbacks
-client.on('connect', function () {
-    console.log('Connected');
-    client.subscribe('etoll/rfid', {qos: 1}, function(){
-        console.log('subscribed')
-      });
+client.on("connect", function () {
+  console.log("Connected");
+  client.subscribe("etoll/rfid", { qos: 1 }, function () {
+    console.log("subscribed");
+  });
 });
 
-client.on('error', function (error) {
-    console.log(error);
+client.on("error", function (error) {
+  console.log(error);
 });
 
-client.on('message', function (topic, message) {
-    var rfid = message.toString()
-    
-    // called each time a message is received
-    
-    // arr.push(rfid)
-    // if(rfid.length > 0){
-      
-      count++
-      new Promise ((res, rej)=>{
-        data += '['
-        data += rfid
-        data += ','
-        res()
+client.on("message", function (topic, message) {
+  var rfid = message.toString();
 
-      });
-      if(data.length > 0){
-        // arr.push(data)
-        data = data.slice(0, -1)
-        data +=']'
-        data = data.replace('][', ',')
-        let act = JSON.parse(data)
-        console.log(act)
-        console.log('wait...')
-        data += ''
-      }
-      // prevCount = count
-      // for(i=0;i<count;i++){
-      //   console.log('for value: ', i)
-      // }
-      // count=0
-      // if(prevCount === count){
-      //   console.log('counter stopped')
-      // }
-      // console.log('prev: ', prevCount)
-
-      // col(data)
-      // async function val(params) {
-      //   let listener1 = new Promise((resolve, reject) => {
-      //     setTimeout(() => {
-      //       resolve(data);
-      //     }, 1000);
-      //   });
-      //   let dat = await Promise.all(listener1)
-        // console.log(data)
-
-
-
+  count++;
+  new Promise((res, rej) => {
+    data += "[";
+    data += rfid;
+    data += ",";
+    res();
+  });
+  if (data.length > 0) {
+    // arr.push(data)
+    data = data.slice(0, -1);
+    data += "]";
+    data = data.replace("][", ",");
+    let act = JSON.parse(data);
+    console.log(act);
+    console.log("wait...");
+    data += "";
+  }
 });
-
-
 
 app.listen(3000, () => {
-    console.log('Sever Running at port 3000')
-})
+  console.log("Sever Running at port 3000");
+});
 
-async function col(res){
-  console.log(res)
+async function col(res) {
+  console.log(res);
 }
-
 
 //RFID 1
 // cc ff ff 20 5 10 0 30 0 e2 80 69 95 0 0 40 6 bc 59 85 2 bc d3
@@ -104,11 +73,34 @@ async function col(res){
 // // RFID 4
 // cc ff ff 20 5 10 0 30 0 e2 0 0 20 66 11 2 72 18 40 ec b4 bd 2f
 
-
-
 // module.exports = {
 //     apps : [{
 //       name   : "etollbroker",
 //       script : "npm start"
 //     }]
 //   }
+
+// called each time a message is received
+
+// arr.push(rfid)
+// if(rfid.length > 0){
+
+// prevCount = count
+// for(i=0;i<count;i++){
+//   console.log('for value: ', i)
+// }
+// count=0
+// if(prevCount === count){
+//   console.log('counter stopped')
+// }
+// console.log('prev: ', prevCount)
+
+// col(data)
+// async function val(params) {
+//   let listener1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(data);
+//     }, 1000);
+//   });
+//   let dat = await Promise.all(listener1)
+// console.log(data)
--- test.js
+++ test.js
@@ -1,39 +1,38 @@
-const mqtt = require('mqtt')
-require('dotenv').config();
-const express = require('express')
-const app = express()
+const mqtt = require("mqtt");
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
 var options = {
-    host: process.env.MQTT_HOST,
-    port: process.env.MQTT_PORT,
-    protocol: process.env.MQTT_PROTOCAL,
-    username: process.env.MQTT_USERNAME,
-    password: process.env.MQTT_PASSWORD,
-    clientId: 'mqttjs_pappi2',
-    clean: false,
-}
+  host: process.env.MQTT_HOST,
+  port: process.env.MQTT_PORT,
+  protocol: process.env.MQTT_PROTOCAL,
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD,
+  clientId: "mqttjs_pappi2",
+  clean: false,
+};
 
 // initialize the MQTT client
 var client = mqtt.connect(options);
 
+client.on("connect", function () {
+  console.log("Connected");
+  // client.subscribe({topic: 'etoll/carid', options:{qos: 1}});
+  // client.subscribe({topic: 'etoll/carid', function (err) {
+  //     console.log(err)
+  //     console.log('Subscribed')
+  // }})
+});
+client.subscribe("etoll/carid", function () {
+  console.log("subscribe");
+});
 
-client.on('connect', function () {
-    console.log('Connected');
-    // client.subscribe({topic: 'etoll/carid', options:{qos: 1}});
-    // client.subscribe({topic: 'etoll/carid', function (err) {
-    //     console.log(err)
-    //     console.log('Subscribed')
-    // }})
-  })
-  client.subscribe('etoll/carid', function(){
-    console.log('subscribe')
-  });
-  
-  client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString())
-    // client.end()
-  })
-  app.listen(3000, () => {
-    console.log('Sever Running at port 3000')
-})
+client.on("message", function (topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+  // client.end()
+});
+app.listen(3000, () => {
+  console.log("Sever Running at port 3000");
+});
